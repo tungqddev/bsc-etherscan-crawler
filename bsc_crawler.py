99a79335d4868c159deb71b343f434b2414b2bf0
Tung Quach Dai <quachdaitung@gmail.com>
2021-12-01 19:14:01 +0700

    Init project for crawler

---
+++ bsc_crawler.py
@@ -0,0 +1,137 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.firefox.options import Options
+from typing import Any, Dict, List, Optional, Type, Union
+import time
+import csv
+import argparse
+import sys
+
+options = Options()
+options.headless = True
+
+driver = webdriver.Firefox(options=options,executable_path = '/Users/macpro/Downloads/geckodriver')
+# driver = webdriver.Firefox(executable_path = '/Users/macpro/Downloads/geckodriver')
+driver.implicitly_wait(0)
+token_list_file_path = './token_list.csv'
+
+class BSCCrawler:
+
+  def __init__(self, from_page, to_page) -> None:
+    MAX_PAGE_NUM = 10
+    
+    smart_contract_list = self.get_token_address_list(token_list_file_path)
+    if smart_contract_list is not None:
+      for addr in smart_contract_list.keys():
+        self.get_holder_information(from_page, to_page, smart_contract_list[addr], addr)
+
+  def get_holder_information(self, from_page: int, to_page: int, smart_contract_address_url: str, holder_list_file_path) -> None:
+    
+    # driver.get("https://etherscan.io/token/0xc4c7ea4fab34bd9fb9a5e1b1a98df76e26e6407c")
+    driver.get(smart_contract_address_url)
+    print('start to load holder list and insert to the file {} from page {} to page {}'.format(holder_list_file_path, from_page, to_page))
+    element = WebDriverWait(driver,10).until(EC.presence_of_element_located(
+      (By.XPATH, '//*[@id="ContentPlaceHolder1_tabHolders"]')))
+
+    driver.execute_script("arguments[0].click();", element)
+
+    holder_list_table_frame = WebDriverWait(driver,10).until(EC.presence_of_element_located(
+      (By.XPATH, '//*[@id="tokeholdersiframe"]')))
+
+    driver.switch_to.frame(holder_list_table_frame)
+    # self.create_holder_list_header()
+    index = 0
+    for x in range(0, to_page):
+      if index < from_page -1:
+        print("start to load holder list page ", index)
+        next_anchor = WebDriverWait(driver,10).until(EC.presence_of_element_located(
+        (By.XPATH, '/html/body/div[2]/div[2]/nav/ul/li[4]/a')))
+        driver.execute_script(next_anchor.get_attribute('href'))
+        # ActionChains(driver).move_to_element(next_anchor).click().perform()
+        time.sleep(3)
+        index+=1
+        continue
+
+      time.sleep(3)
+      self.crawl_holder_information(holder_list_file_path)
+      next_anchor = WebDriverWait(driver,10).until(EC.presence_of_element_located(
+      (By.XPATH, '/html/body/div[2]/div[2]/nav/ul/li[4]/a')))
+      if next_anchor is not None:
+        driver.execute_script(next_anchor.get_attribute('href'))
+        index+=1
+
+      # ActionChains didn't work in the headless mode
+      # ActionChains(driver).move_to_element(next_anchor).click().perform()
+    driver.switch_to_default_content
+
+  # Collect holder address from the token
+  def crawl_holder_information(self, holder_file_name: str) -> None:
+      holder_list_table = WebDriverWait(driver, 10).until(
+          EC.presence_of_element_located((By.ID, "maintable")))
+      rows = holder_list_table.find_elements(By.TAG_NAME, "tr")
+      with open(holder_file_name, 'a', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        for row in rows[1:]:
+            cols = row.find_elements(By.TAG_NAME, "td")
+            col_list = []
+            for col in cols:
+              anchor_tag = col.find_elements(By.TAG_NAME, "a")
+              if len(anchor_tag) > 0:
+                print(anchor_tag[0].get_attribute("href"))
+                col_list.append(anchor_tag[0].get_attribute("href"))
+              else:
+                col_list.append(col.text)
+            writer.writerow(col_list)
+        f.close
+
+  # Get token address list from file
+  def get_token_address_list(self, file_path: str) -> any:
+    token_address_dict = {}
+    list_token = []
+    with open('./token_list.csv', mode='r') as csv_file:
+      csv_reader = csv.DictReader(csv_file)
+      line_count = 0
+      for row in csv_reader:
+          token_file_name = self.create_symbol_csv(row["Symbol"])
+          
+          if line_count == 0:
+              line_count += 1
+          list_token.append(row["Smart Contract"])
+          token_address_dict[token_file_name] = row["Smart Contract"]
+          line_count += 1
+    
+    print('There are {} tokens in the list'.format(len(list_token)))
+    return token_address_dict
+
+  # Create file output csv file for each symbol
+  def create_symbol_csv(self, symbol: str) -> str:
+    if symbol is None:
+      return None
+    token_holder_filename = symbol + "_token_holder.csv"
+
+    print('create file {}'.format(token_holder_filename))
+    with open(token_holder_filename, mode='w', encoding='UTF8') as f:
+      writer = csv.writer(f)
+      holder_address_header = ['Rank', 'Address',
+                               'Quantity', 'Percentage', 'Value', 'Analytics']
+      writer.writerow(holder_address_header)
+      f.close
+    return token_holder_filename
+  
+# Craw token holder list
+parser = argparse.ArgumentParser(description='Crawl token holder information')
+parser.add_argument('-from','--from', help='From holder page index', required=True)
+parser.add_argument('-to','--to', help='To holder page index', required=True)
+args = vars(parser.parse_args())
+
+from_page = args['from']
+to_page = args['to']
+
+BSCCrawler(int(from_page),int(to_page))
+# BSCCrawler(5,10)
+
+driver.close()
---
+++ bsc_holder_info_crawler.py
@@ -0,0 +1,117 @@
+from builtins import print
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.firefox.options import Options
+from typing import Any, Dict, List, Optional, Type, Union
+import csv
+import re
+import time
+
+class BSCHolderInfoCrawler:
+
+    def __init__(self) -> None:
+        MAX_PAGE_NUM = 10
+        
+        holder_address = self.get_holder_address_list('')
+        skip_index = 1
+        index = 1
+        for address in holder_address:
+            try:
+                if index < skip_index:
+                    index = index + 1
+                    continue
+                print("reading line ", index)
+                print("getting information from ", address)
+                self.get_list_token_of_holder(address)
+                index = index + 1
+                
+            except Exception:
+                print("can't load information of holder ", address)
+                continue
+
+        # self.get_list_token_of_holder(holder_address)
+
+    def get_list_token_of_holder(self, holder_address) -> None:
+
+        options = Options()
+        options.headless = True
+        holder_info_driver = webdriver.Firefox(options=options,
+            executable_path='/Users/macpro/Downloads/geckodriver')
+        holder_info_driver.implicitly_wait(10)
+        holder_info_driver.get(holder_address)
+
+        # click on holder address
+        xpath_navigation_anchor = re.sub(r'.+\?.=', '', holder_address)
+        holder_navigation_anchor = WebDriverWait(holder_info_driver, 10).until(EC.presence_of_all_elements_located((
+            By.XPATH, '//*[@id="spanTarget_' + xpath_navigation_anchor + '"]')))
+        if holder_navigation_anchor is None:
+            return
+        # go to the details of holder
+        holder_info_driver.get(
+            holder_navigation_anchor[0].get_attribute('href'))
+
+        # anchor_tag = holder_navigation_anchor[0].find_elements(By.TAG_NAME, "a")
+        token_list_dropdown = WebDriverWait(holder_info_driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//*[@id="availableBalanceDropdown"]')))
+
+        time.sleep(2)
+        if token_list_dropdown is None:
+            return
+        ActionChains(holder_info_driver).move_to_element(
+            token_list_dropdown[0]).click().perform()
+
+        holder_list_token_ul = WebDriverWait(holder_info_driver, 10).until(EC.presence_of_all_elements_located(
+            (By.XPATH, '/html/body/div[1]/main/div[4]/div[1]/div[1]/div/div[2]/div[3]/div/div[2]/div/div/div[2]/div[1]/div/ul')))
+
+        holder_list_token_li_list = holder_list_token_ul[0].find_elements(
+            By.TAG_NAME, "li")
+        # [0].find_elements(By.TAG_NAME, "span")
+        with open('holder_tokens_info.csv', 'a', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            for li in holder_list_token_li_list[1:]:
+                total_span_tag = li.find_elements(By.TAG_NAME, "span")
+                if len(total_span_tag) < 3:
+                    continue
+                token_span_1 = li.find_elements(By.TAG_NAME, "span")[1].text
+                token_span_2 = li.find_elements(By.TAG_NAME, "span")[2].text
+                token_span_1_split = token_span_1.split(" ")
+                if len(token_span_1_split) > 1:
+                    total_in_dolars = re.sub(',', '', token_span_2[1:])
+                    if isfloat(total_in_dolars):
+                        if token_span_1_split[1] == 'ETH':
+                            continue
+                        if float(total_in_dolars) > 100000:
+                            csv_row = []
+                            csv_row.append(xpath_navigation_anchor)
+                            csv_row.append(token_span_1_split[1])
+                            csv_row.append(total_in_dolars)
+                            writer.writerow(csv_row)
+            f.close
+        holder_info_driver.close()
+
+    def get_holder_address_list(self, file_path: str) -> List[str]:
+        list_address = []
+        with open('./holder_list.csv', mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                list_address.append(row["Address"])
+                line_count += 1
+        print("There are [{}] holder in the file", len(list_address))
+        return list_address
+
+
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
+BSCHolderInfoCrawler()
